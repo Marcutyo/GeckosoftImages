5010b357342e83ffc183bc1412e055d74f152f57
Marco Palmisano <78793610+Marcutyo@users.noreply.github.com>
2022-10-22 17:35:32 +0200

    use native Image from System.Drawing to handle images

--- GeckosoftImages/Interfaces/IImageService.cs
+++ GeckosoftImages/Interfaces/IImageService.cs
@@ -6,5 +6,6 @@ namespace GeckosoftImages.Interfaces
     public interface IImageService
     {
         Task<ImageResponse> UploadImage(ImageRequest imageRequest);
+        Task<ImageResponse> ResizeImage(string name, int width, int height);
     }
 }
--- GeckosoftImages/Services/ImageService.cs
+++ GeckosoftImages/Services/ImageService.cs
@@ -2,6 +2,9 @@
 using GeckosoftImages.Requests;
 using GeckosoftImages.Responses;
 using GeckosoftImages.Interfaces;
+using System.Drawing;
+using GeckosoftImages.Exceptions;
+using System.Drawing.Imaging;
 
 namespace GeckosoftImages.Services
 {
@@ -25,6 +28,33 @@ namespace GeckosoftImages.Services
             await imageRequest.Image.CopyToAsync(new FileStream(filePath, FileMode.Create));
 
             return new ImageResponse { Success = true, FilePath = filePath };
+        }
+
+        public Task<ImageResponse> ResizeImage(string name, int width, int height)
+        {
+            var image = GetImageByName(name);
+            var resizedImage = new Bitmap(image, new Size(width, height));
+
+            using var imageStream = new MemoryStream();
+            resizedImage.Save(imageStream, ImageFormat.Jpeg);
+            var imageBytes = imageStream.ToArray();
+
+            throw new NotImplementedException();
+        }
+
+        private Image GetImageByName(string name)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+
+            string[] imagePaths = Directory.GetFiles(uploads, searchPattern: name + ".*");
+            if (imagePaths.Length > 1)
+            {
+                throw new TooManyFilesException();
+            }
+            string imagePath = imagePaths.First();
+
+            var image = Image.FromStream(new FileStream(imagePath, FileMode.Open));
+            return image;
         }
     }
 }

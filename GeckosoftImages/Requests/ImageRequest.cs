7c5c68524e96bfdb2176ad15fc7f31d029b6111e
Marco Palmisano <78793610+Marcutyo@users.noreply.github.com>
2022-10-23 19:55:35 +0200

    add constructor

--- GeckosoftImages/Requests/ImageRequest.cs
+++ GeckosoftImages/Requests/ImageRequest.cs
@@ -1,10 +1,15 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace GeckosoftImages.Requests
 {
     public class ImageRequest
     {
+        [Required]
         public IFormFile Image { get; set; }
 
+        public ImageRequest(IFormFile image)
+        {
+            Image = image;
+        }
     }
 }

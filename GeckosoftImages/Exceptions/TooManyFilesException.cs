b255667babb872d9037ee5b668f35b5ab4dec9f9
Marco Palmisano <78793610+Marcutyo@users.noreply.github.com>
2022-10-23 15:44:36 +0200

    add custom exception when there is more than 1 image with the same file name (without ext)

---
+++ GeckosoftImages/Exceptions/TooManyFilesException.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace GeckosoftImages.Exceptions
+{
+    public class TooManyFilesException : Exception
+    {
+        public TooManyFilesException()
+        {
+        }
+
+        public TooManyFilesException(string? message) : base(message)
+        {
+        }
+
+        public TooManyFilesException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}

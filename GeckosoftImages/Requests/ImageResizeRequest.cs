b56701f23d87dcc63bb2f1750383dedb90246ba8
Marco Palmisano <78793610+Marcutyo@users.noreply.github.com>
2022-10-24 02:47:53 +0200

    create ImageResizeRequest

---
+++ GeckosoftImages/Requests/ImageResizeRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace GeckosoftImages.Requests
+{
+    public class ImageResizeRequest
+    {
+        public string ImagePath { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public ImageResizeRequest(string fileName, int width, int height)
+        {
+            ImagePath = fileName;
+            Width = width;
+            Height = height;
+        }
+    }
+}

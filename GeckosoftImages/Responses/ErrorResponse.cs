d3b5fd2861998047831afd9dded73a920b9c67f3
Marco Palmisano <78793610+Marcutyo@users.noreply.github.com>
2022-10-24 02:56:21 +0200

    delete string Type from ErrorResponse

--- GeckosoftImages/Responses/ErrorResponse.cs
+++ GeckosoftImages/Responses/ErrorResponse.cs
@@ -3,12 +3,10 @@
     public class ErrorResponse
     {
         public bool Success { get; set; } = false;
-        public string Type { get; set; }
         public string Detail { get; set; }
 
         public ErrorResponse(Exception e)
         {
-            Type = e.Source;
             Detail = e.Message;
         }
     }
